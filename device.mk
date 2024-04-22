d4254d9e702e64f66d5dbb0f860f5938f52d351e
jmpfbmx <jmpf.bmx@gmail.com>
2024-04-22 17:37:33 +0200

    aaron: Import WiFi configs
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: Ibbcdbe9caf027e317a26aa147ec5bc39c40b05f2

--- device.mk
+++ device.mk
@@ -132,3 +132,9 @@ PRODUCT_SHIPPING_API_LEVEL := 28
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+
+# WiFi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
+    $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
