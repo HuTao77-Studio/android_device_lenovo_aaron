ae99fb1361ebcb2812f77f16a5a7aff1663a66d0
Zinadin Zidan <zidan44@pixelexperience.org>
2024-07-13 14:11:39 +0200

    aaron: Patchelf MediaTek WiFi HAL
    * Load libwifi-hal-mtk.so instead of libwifi-hal.so [We cant build libwifi-hal from aosp as it will interrupt MTK WiFi services so lets rename & pin]
    * Also load libwifi-hal-mtk.so in android.hardware.wifi@1.0-service-mediatek
    
    Change-Id: Ia08378d8495f825dfdb2fed7ca5154cfdd9390cc

--- extract-files.sh
+++ extract-files.sh
@@ -58,6 +58,9 @@ setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
 function blob_fixup() {
     case "${1}" in
+        vendor/bin/hw/android.hardware.wifi@1.0-service-lazy-mediatek)
+            "${PATCHELF}" --replace-needed "libwifi-hal.so" "libwifi-hal-mtk.so" "${2}"
+            ;;
         vendor/lib*/libutinterface_custom_md.so)
             "${PATCHELF}" --add-needed libutinterface_md.so "${2}"
             ;;
