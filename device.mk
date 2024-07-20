ddca0128b72644ac66c859ac7791c366c4bb1e8f
jmpfbmx <jmpf.bmx@gmail.com>
2024-07-20 11:47:00 +0200

    aaron: Switch to our custom audio impl
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: Ia232f7e962b542dffc892b48ffffe322dab0edd4

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.common@5.0-util.vendor \
     android.hardware.audio.common@5.0.vendor \
     android.hardware.audio.effect@5.0.vendor \
-    android.hardware.audio.effect@7.1-impl \
+    android.hardware.audio.effect@7.1-impl-mediatek \
     android.hardware.audio.service \
     android.hardware.audio@5.0.vendor \
     android.hardware.audio@7.0-impl \
